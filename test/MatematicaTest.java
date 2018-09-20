c2b39d823367af48b7341e7c9fb8485e9a3186ef
cavanhaman <cavanhaman@hotmail.com>
2018-09-19 22:31:40 -0300

    Varios testes adicionados com resultados distintos

--- test/MatematicaTest.java
+++ test/MatematicaTest.java
@@ -1,15 +1,36 @@
+/*********************
+* Rodrigo CavanhaMan *
+*        IFTM        *
+*********************/
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MatematicaTest {
 
-	public static void main (String args[]) {
+/*	public static void main (String args[]) {
 		MatematicaTest t = new MatematicaTest();
 		t.testaFatorialZero();
-	}
+	}*/
+	@Test
 	public void testaFatorialZero() {
 		Matematica matematica = new Matematica();
 		int resposta = matematica.fatorial(0);
-		if (resposta==1)
-			System.out.println("Batuta!");
-		else
-			System.out.println("Faiô!");
+		Assert.assertEquals(resposta, 1);
 	}
+
+	@Test
+	public void testaFatorialUm() {
+		Matematica matematica = new Matematica();
+		int resposta = matematica.fatorial(-5);
+		Assert.assertEquals(resposta, 1);
+	}
+
+	@Test
+	public void testaFatorialCinco() {
+		Matematica matematica = new Matematica();
+		int resposta = matematica.fatorial(1);
+		Assert.assertEquals(resposta, 2);
+	}
+
+
 }
