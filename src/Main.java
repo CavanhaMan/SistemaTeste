a073dae351e366a7b3789342c558319970567a31
cavanhaman <cavanhaman@hotmail.com>
2018-09-19 22:02:43 -0300

    Classes iniciais

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
++------------------+
+|Rodrigo CavanhaMan|
+|       IFTM       |
++------------------+
+*/
+import java.util.Scanner;
+import java.util.Locale;
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(new Locale("en", "US"));
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+}
---
+++ src/Matematica.java
@@ -0,0 +1,12 @@
+/*********************
+* Rodrigo CavanhaMan *
+*        IFTM        *
+*********************/
+public class Matematica {
+
+	public int fatorial(int n) {
+		if(n==0 || n==1)
+			return 1;
+		return n * fatorial(n-1);
+	}
+}
