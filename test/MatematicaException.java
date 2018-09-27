81d7cef68235659db68524a05151b9176bcdabcb
cavanhaman <cavanhaman@hotmail.com>
2018-09-26 21:49:08 -0300

    Adicionando Exception para tratamento de erro e mais um caso de teste

---
+++ test/MatematicaException.java
@@ -0,0 +1,8 @@
+
+public class MatematicaException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public MatematicaException(String mensagem) {
+		super(mensagem);
+	}
+}
--- test/MatematicaTest.java
+++ test/MatematicaTest.java
@@ -3,33 +3,43 @@
 *        IFTM        *
 *********************/
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MatematicaTest {
 
+	private Matematica matematica;
+	
+	@Before
+	public void init() {
+		matematica = new Matematica();
+	}
 /*	public static void main (String args[]) {
 		MatematicaTest t = new MatematicaTest();
 		t.testaFatorialZero();
 	}*/
 	@Test
-	public void testaFatorialZero() {
-		Matematica matematica = new Matematica();
+	public void testaFatorialZero() throws MatematicaException {
 		int resposta = matematica.fatorial(0);
 		Assert.assertEquals(resposta, 1);
 	}
 
 	@Test
-	public void testaFatorialUm() {
-		Matematica matematica = new Matematica();
+	public void testaFatorialUm() throws MatematicaException {
 		int resposta = matematica.fatorial(-5);
 		Assert.assertEquals(resposta, 1);
 	}
 
 	@Test
-	public void testaFatorialCinco() {
-		Matematica matematica = new Matematica();
+	public void testaFatorialCinco() throws MatematicaException {
 		int resposta = matematica.fatorial(1);
 		Assert.assertEquals(resposta, 2);
+	}
+
+	@Test
+	public void testaFatorialtreze() throws MatematicaException {
+		int resposta = matematica.fatorial(1);
+		Assert.assertEquals(resposta, 62270208);
 	}
 
 
